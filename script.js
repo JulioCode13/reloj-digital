f632ca10f5e6d1d8f59b035da7b112d335dab8c6
Julio Peña <juliotiti21@gmail.com~>
2024-02-24 17:08:48 -0500

    final commit

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ let fechaActual = new Date();
 let horaactual = fechaActual.getHours();
 
 if (horaactual >= 19 || horaactual < 6){
-    body.style.backgroundImage = "url('Noche.png')"
+    body.style.backgroundImage = "url('https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/panama/Panama_Ciudad_0218_c9f9fbc0-60b3-439b-b75a-1a0c87f76622.jpg')"
 }else{
     body.style.backgroundImage = "url('https://www.repsol.com/content/dam/repsol-corporate/es/sostenibilidad/naturaleza%20bosque%20y%20lago.jpg.transform/rp-rendition-md/image.jpg')"
 }
